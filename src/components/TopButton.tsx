94fe698f86746fa5ef40501de838ff7b9c9d7abc
jeong922 <hwajeong922@naver.com>
2023-09-06 14:46:27 +0900

    TopButton : 애니메이션 추가

--- src/components/TopButton.tsx
+++ src/components/TopButton.tsx
@@ -24,15 +24,13 @@ export default function TopButton() {
   }, []);
 
   return (
-    <>
-      {showButton && (
-        <button
-          onClick={scrollToTop}
-          className='sticky z-10 float-right p-2 bg-white border rounded-full border-neutral-300 right-8 bottom-8 text-neutral-600 text-2xl shadow-lg'
-        >
-          <MdKeyboardArrowUp />
-        </button>
-      )}
-    </>
+    <button
+      onClick={scrollToTop}
+      className={`${
+        showButton ? 'scale-100' : 'scale-0'
+      } sticky z-10 float-right p-2 bg-white border rounded-full border-neutral-300 right-8 bottom-8 text-neutral-600 text-2xl shadow-lg transition-all duration-300 ease-in-out`}
+    >
+      <MdKeyboardArrowUp />
+    </button>
   );
 }
